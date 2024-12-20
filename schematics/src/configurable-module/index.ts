7eb2956cd9c86f475a51e8887901605f03f742f5
nhaserey <sereynha473@gmail.com>
2024-12-20 14:51:52 +0700

    update schematic

--- schematics/src/configurable-module/index.ts
+++ schematics/src/configurable-module/index.ts
@@ -3,17 +3,78 @@ import {
   externalSchematic,
   Rule,
   SchematicContext,
+  strings,
   Tree,
+  apply,
+  template,
+  move,
+  url,
+  mergeWith, SchematicsException,
 } from '@angular-devkit/schematics';
+import { dasherize } from '@angular-devkit/core/src/utils/strings';
+import * as ts from '@schematics/angular/third_party/github.com/Microsoft/TypeScript/lib/typescript';
+import { findNodes, insertImport } from '@schematics/angular/utility/ast-utils';
+import { InsertChange } from '@schematics/angular/utility/change';
+
+interface ConfigurableModuleSchematicOptions {
+  name: string;
+}
+
+function updateModuleFile(
+  tree: Tree,
+  options: ConfigurableModuleSchematicOptions,
+): Tree {
+  const name = dasherize(options.name);
+  const moduleFilePath = `src/${name}/${name}.module.ts`;
+  if (!tree.exists(moduleFilePath)) {
+    throw new SchematicsException(
+      `Module file ${moduleFilePath} does not exist.`,
+    );
+  }
+  const moduleFileContent = tree.readText(moduleFilePath);
+  const source = ts.createSourceFile(
+    moduleFilePath,
+    moduleFileContent,
+    ts.ScriptTarget.Latest,
+    true,
+  );
+  const updateRecorder = tree.beginUpdate(moduleFilePath);
+  const insertImportChange = insertImport(
+    source,
+    moduleFilePath,
+    'ConfigurableModuleClass',
+    `./${name}.module-definition`,
+  );
+  if (insertImportChange instanceof InsertChange) {
+    updateRecorder.insertRight(
+      insertImportChange.pos,
+      insertImportChange.toAdd,
+    );
+  }
+  const className = findNodes(source, ts.SyntaxKind.ClassDeclaration)[0];
+  updateRecorder.insertRight(
+    className.end - 2,
+    'extends ConfigurableModuleClass',
+  );
+  tree.commitUpdate(updateRecorder);
+  console.log(tree.readText(moduleFilePath));
+  return tree;
+}
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function generate(_options: any): Rule {
+export function generate(options: ConfigurableModuleSchematicOptions): Rule {
   return (_tree: Tree, _context: SchematicContext) => {
+    const templateSource = apply(url('./files'), [
+      template({ ...options, ...strings }),
+      move('src'),
+    ]);
     return chain([
       externalSchematic('@nestjs/schematics', 'module', {
-        name: 'placeholder',
+        name: options.name,
       }),
+      mergeWith(templateSource),
+      (tree: Tree) => updateModuleFile(tree, options),
     ]);
   };
 }
